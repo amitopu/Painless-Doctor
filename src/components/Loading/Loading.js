51d1991677c1e83544bd3adcd60d876e30c9db88
rupok <nurealamsiddiky@gmail.com>
2022-04-19 22:33:13 +0600

    loading while reload is functional

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className="text-4xl italic text-sky-800 mx-auto mt-10">
+            Loading....
+        </div>
+    );
+};
+
+export default Loading;
